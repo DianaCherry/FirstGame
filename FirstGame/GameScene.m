32bf04a5a53cda21219e6c9511da7be65e4788bc
Diana Krasnozhenova <dianakrasnozhenova@MacBook-Air-Diana.local>
2014-12-24 22:49:29 +0300

    Initial Commit

--- FirstGame/GameScene.m
+++ FirstGame/GameScene.m
@@ -8,18 +8,40 @@
 
 #import "GameScene.h"
 
-@implementation GameScene
+@implementation GameScene {
+    SKNode *_bgLayer;
+    SKNode *_gameLayer;
+    SKNode *_HUDLayer;
+    NSTimeInterval *_dt;
+    NSTimeInterval *_lastUpdateTime;
+}
 
 -(void)didMoveToView:(SKView *)view {
     /* Setup your scene here */
     SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
     
-    myLabel.text = @"Hello, World!";
-    myLabel.fontSize = 65;
-    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                   CGRectGetMidY(self.frame));
+    _bgLayer = [SKNode node];
+    [self addChild:_bgLayer];
+    _gameLayer = [SKNode node];
+    [self addChild:_gameLayer];
+    _HUDLayer = [SKNode node];
+    [self addChild:_HUDLayer];
     
-    [self addChild:myLabel];
+    SKTexture *backgroundTexture = [SKTexture textureWithImageNamed:@"Background"];
+    SKAction *moveBg = [SKAction moveByX:-backgroundTexture.size.width*2 y:0 duration:0.1*backgroundTexture.size.width];
+    SKAction *resetBg = [SKAction moveByX:backgroundTexture.size.width*2 y:0 duration:0];
+    SKAction *moveBgForever = [SKAction repeatActionForever: [SKAction sequence:@[moveBg, resetBg]]];
+    
+    for (int i = 0; i < 2 + self.frame.size.width / (backgroundTexture.size.width * 2); ++i) {
+        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithTexture:backgroundTexture];
+        [sprite setScale:1.0];
+        sprite.zPosition = -20;
+        sprite.anchorPoint = CGPointZero;
+        sprite.position = CGPointMake(i * sprite.size.width, 0);
+        [sprite runAction:moveBgForever];
+        [_bgLayer addChild:sprite];
+        
+    }
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
